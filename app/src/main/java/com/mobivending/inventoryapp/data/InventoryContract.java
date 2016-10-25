1b0fb6df1bbc49bb9ce31d4e6b71874551d27ef3
kobi <kobi@nayax.com>
2016-10-25 19:44:18 +0300

    Initial commit

---
+++ app/src/main/java/com/mobivending/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,114 @@
+package com.mobivending.inventoryapp.data;
+
+/**
+ * Created by kobishasha on 10/25/16.
+ */
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Inventory app.
+ */
+public final class InventoryContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private InventoryContract() {}
+
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.mobivending.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /** Name of database table for pets */
+        public final static String TABLE_NAME = "inventory";
+
+        /**
+         * Unique ID number for the inventory product (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the inventory product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_INVENTORY_NAME ="name";
+
+        /**
+         * Quantity of the inventory product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_INVENTORY_QUANTITY = "quantity";
+
+
+        /**
+         * Price of the inventory product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_INVENTORY_PRICE = "price";
+
+//        /**
+//         * Possible values for the gender of the pet.
+//         */
+//        public static final int GENDER_UNKNOWN = 0;
+//        public static final int GENDER_MALE = 1;
+//        public static final int GENDER_FEMALE = 2;
+
+//        /**
+//         * Returns whether or not the given gender is {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+//         * or {@link #GENDER_FEMALE}.
+//         */
+//        public static boolean isValidGender(int gender) {
+//            if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
+//                return true;
+//            }
+//            return false;
+//        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/mobivending/inventoryapp/data/InventoryDBHelper.java
@@ -0,0 +1,58 @@
+package com.mobivending.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.mobivending.inventoryapp.data.InventoryContract.InventoryEntry;
+
+/**
+ * Created by kobishasha on 10/25/16.
+ */
+
+public class InventoryDBHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = InventoryDBHelper.class.getSimpleName();
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "inventory.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link InventoryDBHelper}.
+     *
+     * @param context of the app
+     */
+    public InventoryDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_INVENTORY_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_INVENTORY_QUANTITY + " INTEGER NOT NULL, "
+                + InventoryEntry.COLUMN_INVENTORY_PRICE + " INTEGER NOT NULL );";
+
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // The database is still at version 1, so there's nothing to do be done here.
+    }
+}
